d02a0c931e290006f5fc10b2fde41a4192554e1d
Tuấn Trần <tuantran280504@gmail.com>
2025-03-25 22:51:24 +0700

    update

--- config/config.php
+++ config/config.php
@@ -1,14 +1,14 @@
 <?php
 $configDB = array();
-$configDB["host"] 		= "localhost";
+$configDB["host"] 		= "host.docker.internal";
 $configDB["database"]	= "computer_store";
 $configDB["username"] 	= "root";
 $configDB["password"] 	= "";
-define("HOST", "localhost");
+$configDB["port"] 	= "3307";
+define("HOST", "host.docker.internal:3308");
 define("DB_NAME", "computer_store");
 define("DB_USER", "root");
 define("DB_PASS", "");
 define('ROOT', dirname(dirname(__FILE__) ) );
 //Thu muc tuyet doi truoc cua config; c:/wamp/www/lab/
-define("BASE_URL", "http://".$_SERVER['SERVER_NAME']);//dia chi website
 ?>
