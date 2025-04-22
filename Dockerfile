eb5183720ade677c1e4e8f6f2f74e468cbf892dc
Tuấn Trần <tuantran280504@gmail.com>
2025-04-22 23:35:43 +0700

    done

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,7 @@
-FROM php:7.4.3-fpm
+FROM php:8.1-apache
 
-RUN apt-get update && apt-get install -y \
-    git \
-    curl \
-    libpng-dev \
-    libonig-dev \
-    libxml2-dev \
-    zip \
-    unzip
+# Cài PDO và MySQL driver
+RUN docker-php-ext-install pdo pdo_mysql
 
-RUN apt-get clean && rm -rf /var/lib/apt/lists/*
-
-RUN pecl install apcu-5.1.20 && docker-php-ext-enable apcu
-
-RUN docker-php-ext-install pdo_mysql mbstring exif pcntl bcmath gd mysqli
-
-WORKDIR /var/www
-
-COPY . /var/www/
-
-RUN chown -R www-data:www-data /var/www
-
-# CMD ["/usr/local/bin/start"]
-CMD ["php-fpm"]
+# Tùy chọn: Copy mã nguồn vào container nếu cần
+COPY . /var/www/html
--- config/config.php
+++ config/config.php
@@ -1,14 +1,14 @@
 <?php
 $configDB = array();
-$configDB["host"] 		= "host.docker.internal";
-$configDB["database"]	= "computer_store";
-$configDB["username"] 	= "root";
-$configDB["password"] 	= "";
-$configDB["port"] 	= "3307";
-define("HOST", "mysql_app_2:3306");
+$configDB["host"] = "host.docker.internal";
+$configDB["database"] = "computer_store";
+$configDB["username"] = "root";
+$configDB["password"] = "";
+$configDB["port"] = "3307";
+define("HOST", "mysql-db:3306");
 define("DB_NAME", "computer_store");
 define("DB_USER", "root");
-define("DB_PASS", "");
-define('ROOT', dirname(dirname(__FILE__) ) );
+define("DB_PASS", "123456");
+define('ROOT', dirname(dirname(__FILE__)));
 //Thu muc tuyet doi truoc cua config; c:/wamp/www/lab/
 ?>
